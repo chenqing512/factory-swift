4344b2ee4d4b7fe24ed3244a72ce7489a3384a27
Qing Chen <chenqing@qq.com>
2018-05-03 17:07:26 +0800

    First commit

---
+++ factoryTests/factoryTests.swift
@@ -0,0 +1,36 @@
+//
+//  factoryTests.swift
+//  factoryTests
+//
+//  Created by Qing Chen on 2018/5/3.
+//  Copyright © 2018年 Qing Chen. All rights reserved.
+//
+
+import XCTest
+@testable import factory
+
+class factoryTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
